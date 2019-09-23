6eb52ef3e8497cc11b4a2868906b223e44721427
Sydney Blom <sydneyblom@#gmail.com>
2019-09-23 15:28:20 -0500

    Navbar with mininmal styling, set up but paths are not linked yet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
 import UserForm from './components/AdminLogin';
-import Navbar from './components/Navbar'
-
 function App() {
   return (
+
   <div className="App">
     <h1>Build Week - Luncher App</h1>
     <UserForm />
