628daa54aca248e7f107e111197a996d14517a40
Diana Grams <deegrams221@gmail.com>
2019-09-23 12:05:01 -0600

    built layout for School Profiles, added Navigation component

--- src/components/SchoolProfiles.js
+++ src/components/SchoolProfiles.js
@@ -3,8 +3,20 @@ import React from "react";
 const SchoolProfile = () => {
   
   return(
-    <div>
-      <h1>School Profile</h1>
+    <div className='school-profile'>
+      <div>
+        <h1>School Profile {/* school name */}</h1>
+        <h2>Location {/* school location */}</h2>
+      </div>
+      <div>
+        <h3>Current Funds: {/* amount */}</h3>
+        <h4>Amount Requested: {/* amount */}</h4>
+      </div>
+      <div>
+        <button>add</button>
+        <button>edit</button>
+        <button>delete</button>
+      </div>
     </div>
   );
 };
