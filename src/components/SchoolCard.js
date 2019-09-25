d4db6aa6ac66460c765f027d53cfaad7e19dc5bb
Diana Grams <deegrams221@gmail.com>
2019-09-24 18:19:33 -0600

    added edit/cancel edit/ delete buttons to schoolCard

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,6 +1,7 @@
   
-import React from 'react';
+import React, { useState } from 'react';
 import style from 'styled-components';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const Wrapper = style.div`
 display: flex;
@@ -20,19 +21,71 @@ const Description = style.p`
   text-align: center;
 `;
 
-const SchoolCard = function(props)  { 
-    return(
-        <div>
-            <Wrapper>
-            <h3>{props.Name}</h3>
-            <Description>
-            <p>{props.Location}</p>
-            <p>Requested Amount: {props.Funds}</p>
-            </Description>
-            </Wrapper>
-        </div>
+const initialAmount = {amount: ''};
+
+const SchoolCard = function({amounts, updateAmounts, ...props})  {
+  const [editing, setEditing] = useState(false);
+  const [amountToEdit, setAmountToEdit] = useState(initialAmount);
+
+  const editAmount = amount => {
+    setEditing(true);
+    setAmountToEdit(amount);
+  };
+
+  const saveEdit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .put(`/schools/${amountToEdit.id}`, amountToEdit)
+      .then(res => {
+        updateAmounts(amounts.map(amount => {
+          if(amount.id === res.data.id) {
+            return res.data;
+          } else {
+            return amount;
+          }
+        }));
+      })
+      .catch(error => console.log(error));
+  };
+
+  const deleteAmount = amount => {
+    axiosWithAuth()
+      .delete(`/schools/${amount.id}`)
+      .then(res => {
+        updateAmounts(amounts.filter(amount => amount.id !== res.data))
+      })
+      .catch(error => console.log(error));
+  };
+
+  return(
+    <div>
+      <Wrapper>
+        <h3>{props.Name}</h3>
+        <Description>
+          <p>{props.Location}</p>
+          <p>Requested Amount: {props.Funds}</p>
+          </Description>
+      </Wrapper>
+
+      {editing && (
+        <form onSubmit={saveEdit}>
+          <legend>Edit Request Amount</legend>
+          <label>
+            Amount:
+            <input onChange={e => setAmountToEdit({ ...amountToEdit, amount: e.target.value })
+              } value={amountToEdit.amount} />
+          </label>
+          <div className="button-row">
+            <button type="submit">save</button>
+            <button onClick={() => setEditing(false)}>cancel</button>
+            <button onClick={() => deleteAmount(amount)}>delete</button>
+          </div>
+        </form>
+      )}
+    </div>
 
 
-    )
+  );
 }
+
 export default SchoolCard;
