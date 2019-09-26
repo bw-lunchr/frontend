75c8b7d3666985d49dad79109e3ee75530331e31
Diana Grams <deegrams221@gmail.com>
2019-09-25 18:28:17 -0600

    working on add school functinality

--- src/components/AdminProfile.js
+++ src/components/AdminProfile.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import FormikDonorForm from './DonorForm';
 import AdminCard from './AdminCard';
 import {NavLink} from 'react-router-dom';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 
-function SchoolGrid() {
+function SchoolGrid(props) {
+  console.log('school props', props);
+  const [schools, setSchools] = useState([]);
+
+  const getSchools = () => {
+    axiosWithAuth()
+      .get(`/admin/1/schools`)
+      .then(res => {
+        setSchools(res.data);
+      })
+      .catch(error => console.log(error.response));
+  }
+
+  useEffect(() => {
+    getSchools();
+  }, [])
+
+  const addSchool = school => {
+    axiosWithAuth()
+      .post(`/admin/1/schools`, school)
+      .then(res => {
+        setSchools(res.data);
+      })
+      .catch(error => console.log(error.response));
+  }
     return (
       <div className="AdminProf">
         <div className="Prof-Nav">
         <NavLink to='EditProfile'>Edit Profile</NavLink>
         </div>
-        <FormikDonorForm />
+        <FormikDonorForm addSchool={addSchool} />
         <AdminCard />
       </div>
     );
@@ -18,4 +43,3 @@ function SchoolGrid() {
 
 
 export default SchoolGrid;
-
